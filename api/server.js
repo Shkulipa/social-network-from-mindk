ad85e37e554e016764b7bd365e9ba832d032f692
Oleksii <caspernumlock@gmail.com>
2020-12-06 18:17:34 +0200

    - homework less-6(03.12.2020) Design Patterns Introd. Middlewares in ExpressJS

--- api/server.js
+++ api/server.js
@@ -1,11 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// env
+require("dotenv").config();
+const host = process.env.HOST;
+const port = process.env.PORT;
+const file = process.env.FILE;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var fs = require('fs');
+const express = require('express');
+const app = express();
+
+class apiClass {
+  constructor(host, port) { 
+    this.host = host; 
+    this.port = port; 
+  };
+}
+
+const varApiClass = new apiClass(host, port);
+
+ // Если есть файл
+app.get('/', (req, res, next) => {
+  fs.readFile(file, 'utf8', function (err, content) {
+    if (err) {
+      next(err) // Pass errors to Express.
+    } else {
+      res.send(content)
+    }
+  })
+});
+
+ // Если нету файла
+/* app.get('/', (req, res, next) => {
+  fs.readFile('data-missing.txt', 'utf8', function (err, content) {
+    if (err) {
+      next(err) // Pass errors to Express.
+    } else {
+      res.send(content)
+    }
+  })
+}); */
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://${varApiClass.host}:${varApiClass.port}`)
+});
+
